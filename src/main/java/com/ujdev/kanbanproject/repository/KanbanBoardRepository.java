a20de9ebd9246016d5d30158f67e0abb270b0f7d
uj-cpu-dev <uj.dev0913@gmail.com>
2024-05-09 17:39:18 +0100

    remove unused import commit

--- src/main/java/com/ujdev/kanbanproject/repository/KanbanBoardRepository.java
+++ src/main/java/com/ujdev/kanbanproject/repository/KanbanBoardRepository.java
@@ -1,7 +1,5 @@
 package com.ujdev.kanbanproject.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
